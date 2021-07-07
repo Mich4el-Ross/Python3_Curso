2c27f2766edcba3111764fdf4e16d3a1c7bd3690
Michael <michael.doug@outlook.com>
2021-07-07 15:39:26 -0300

    Contagem de pares

---
+++ Mundo_2 - Estruturas_de_controle/Aula13 - (For)/ex047.py
@@ -0,0 +1,11 @@
+"""
+Desafio 047
+
+Problema: Crie um progrma que mostre na tela todos os números pares que estão
+          no intervalo de 1 e 50
+"""
+
+print('Os numeros pares entre 1 e 50 são:')
+
+for x in range(0, 51, 2):
+    print(x, end=' ')
