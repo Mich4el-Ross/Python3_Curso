e83caf992f8518a64d1a07809e8cc047a2ddeeb1
Michael <michael.doug@outlook.com>
2021-07-07 15:15:45 -0300

    Somando dois números

---
+++ Mundo_1 - Fundamentos/Aula06 - (Tipos)/ex003.py
@@ -0,0 +1,13 @@
+"""
+Desafio 003
+
+Problema:   Crie um programa que leia dois números e mostre
+            a soma entre eles.
+"""
+
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+add = n1 + n2
+
+print('A soma entre {} e {} resulta em {}'.format(n1, n2, add))
