79bd63e65c809007a801f0e30d8ea575672aa767
Michael <michael.doug@outlook.com>
2021-07-07 15:24:28 -0300

    Tocando um MP3

---
+++ Mundo_1 - Fundamentos/Aula08 - (Modulos)/ex021.py
@@ -0,0 +1,14 @@
+"""
+Desafio 021
+
+Problema:   Faça um programa em que o Python abra e reproduza o áudio
+            de um arquivo MP3.
+"""
+
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+
+while(pygame.mixer.music.get_busy()): pass
