79ff697c4e3de1d4972cc8f9bbd1f5ed3724eed6
Michael <michael.doug@outlook.com>
2021-07-07 15:14:52 -0300

    Respondendo ao Usuário

---
+++ Mundo_1 - Fundamentos/Aula05 - (Inicio)/ex002.py
@@ -0,0 +1,10 @@
+"""
+Desafio 002
+
+Problema:   Faça um programa que leia o nome de uma pessoa e
+            mostre uma mensagem de boas vindas.
+"""
+
+
+name = input('Digite o seu nome: ')
+print('Bem Vindo', name)
